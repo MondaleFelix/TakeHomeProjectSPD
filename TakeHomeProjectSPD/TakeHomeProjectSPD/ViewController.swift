e18da669dcc737bee2cf3312fd9f5c6499617157
MondaleFelix <mondalefelix@yahoo.com>
2020-06-02 18:08:48 -0700

    removed storyboards

---
+++ TakeHomeProjectSPD/TakeHomeProjectSPD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TakeHomeProjectSPD
+//
+//  Created by Mondale on 6/2/20.
+//  Copyright © 2020 Mondale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
